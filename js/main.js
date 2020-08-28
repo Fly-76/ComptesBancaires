64a59e4c37eeb26860324a6b32d5be3a15e0412c
Fly-76 <58425266+Fly-76@users.noreply.github.com>
2020-08-28 19:53:50 +0200

    Add rules.json

--- js/main.js
+++ js/main.js
@@ -1,11 +1,40 @@
 console.log("loaded");
 
-document.onload = function(){
+// 
+function displayRules(rules) {
+    let html = "";
+    for (let rule of rules) {
+        for (let field in rule)
+            html += `<p>${rule[field]}</p>`;
+    }
+    let splash = document.getElementById("rules");
+    console.log(splash);
+    console.log(html);
 
-};
+//    splash.innerHTML = html;
+}
 
+// load rules from rules.json file using HTTP request
+function loadContent() {
+    httpRequest = new XMLHttpRequest();
+
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+                let rules = JSON.parse(httpRequest.responseText);
+                displayRules(rules);
+            } else {
+                alert("Une erreur est survenue");
+            }
+        }
+    };
+
+    httpRequest.open('GET', 'rules.json', true);
+    httpRequest.send();
+}
+
+// Show splash screen
 $(document).ready(function(){
+    loadContent();
     $("#rules").modal();
 });
-
-
