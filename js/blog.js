9917f271dddd71127c58049f719be6ecd386b872
Fly-76 <58425266+Fly-76@users.noreply.github.com>
2020-08-29 18:32:47 +0200

    Update load blog articles from rest api

---
+++ js/blog.js
@@ -0,0 +1,55 @@
+// get "section" object
+let section = document.getElementsByTagName("section")[0];
+
+/* Display articles
+
+    <div class ="col-12 col-sm-6 col-lg-3">
+        <article class="card h-100">
+            <h4 class="card-title text-center">ARTICLE TITLE</h4>
+            <p class="card-text text-center">ARTICLE CONTENT</p>
+        </article>
+    </div>
+*/
+function displayArticles(articles) {
+    console.log(articles);
+    for (let content of articles) {
+
+        let div = document.createElement("div");
+        div.classList.add("col-12", "col-sm-6", "col-lg-3");
+
+        section.appendChild(div);
+        let article = document.createElement("article");
+        article.classList.add("card", "h-100");
+
+        div.appendChild(article);
+        let h = document.createElement("h4");
+        h.classList.add("card-title", "text-center");
+        h.innerText = content.titre;
+        article.appendChild(h);
+
+        let p = document.createElement("p");
+        p.classList.add("card-text", "text-center");
+        p.innerText = content.contenu;
+        article.appendChild(p);
+    }
+}
+
+// Load articles from https://oc-jswebsrv.herokuapp.com/api/articles
+function loadContent() {
+    httpRequest = new XMLHttpRequest();
+
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+                let articles = JSON.parse(httpRequest.responseText);
+                displayArticles(articles);
+            } else {
+                alert("Une erreur est survenue");
+            }
+        }
+    };
+
+    httpRequest.open('GET', 'https://oc-jswebsrv.herokuapp.com/api/articles', true);
+    httpRequest.send();
+}
+loadContent();
